766157f85d49fd9c644fe8dddfd16347a4b4bf27
VarBoff <vinirechboff@gmail.com>
2022-05-02 13:47:45 -0300

    voiceStateUpdate event creation, log to console whenever user joins or leaves channels. Also updated intents to be able to get voiceStateUpdates

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const Discord = require("discord.js");
 const fs = require("node:fs");
 const { Client, Collection, Intents } = require("discord.js");
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_VOICE_STATES",
+        "GUILD_MESSAGES",
+    ],
+});
 
 // Command Handling
 client.commands = new Collection();
@@ -15,15 +22,15 @@ for (const file of commandFiles) {
 }
 
 // Event Handling
-const eventFiles = fs.readdirSync('./events');
+const eventFiles = fs.readdirSync("./events");
 
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
 }
 
 client.login(token);
