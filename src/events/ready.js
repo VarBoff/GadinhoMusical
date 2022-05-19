5a238443790463bdaaa2596238441ddcd4cc9200
VarBoff <vinirechboff@gmail.com>
2022-05-18 23:57:48 -0300

    changes so i can have a feedback from azure :(

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,18 @@
 module.exports = {
-	name: "ready",
-	once: true,
-	async execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+    name: "ready",
+    once: true,
+    async execute(client) {
+        const express = require("express");
+        const app = express();
+        const port = 3000;
+
+        app.get("/", (req, res) => {
+            res.send("Hello World!");
+        });
+
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+            console.log(`Ready! Logged in as ${client.user.tag}`);
+        });
+    },
 };
